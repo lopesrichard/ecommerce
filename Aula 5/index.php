24e5512bc4ea63688d5778c4bd2b092ae427277b
Richard <contato.richardlopes@outlook.com>
2022-01-29 10:24:13 -0300

    feat: add another product queries

--- Aula 5/index.php
+++ Aula 5/index.php
@@ -8,33 +8,15 @@ require_once 'repo/produtos.php';
 $connection = getConnection();
 
 $produtosEmOferta = getProdutosEmOferta($connection);
+$produtosMaisVendidos = getProdutosMaisVendidos($connection);
+$produtosNovos = getProdutosNovos($connection);
+$produtosRecomendados = getProdutosRecomendados($connection);
 
 $sections = [
   'Ofertas' => $produtosEmOferta,
-  // 'Mais Vendidos' => [
-  //   ['name' => 'Título do Produto', 'rating' => 2, 'value' => 1702, 'discount' => 24, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 4, 'value' => 3555, 'discount' => 23, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 1, 'value' => 6681, 'discount' => 25, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 5, 'value' => 9395, 'discount' => 16, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 3, 'value' => 6145, 'discount' => 11, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 1, 'value' => 2947, 'discount' => 10, 'installments' => 10, 'image' => 'product.png'],
-  // ],
-  // 'Recomendados' => [
-  //   ['name' => 'Título do Produto', 'rating' => 5, 'value' => 6875, 'discount' => 17, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 5, 'value' => 9439, 'discount' => 16, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 4, 'value' => 1043, 'discount' => 16, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 3, 'value' => 3143, 'discount' => 27, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 2, 'value' => 7172, 'discount' => 30, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 3, 'value' => 8479, 'discount' => 30, 'installments' => 10, 'image' => 'product.png'],
-  // ],
-  // 'Novidades' => [
-  //   ['name' => 'Título do Produto', 'rating' => 5, 'value' => 2066, 'discount' => 29, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 2, 'value' => 3669, 'discount' => 23, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 3, 'value' => 1995, 'discount' => 23, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 1, 'value' => 7675, 'discount' => 15, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 4, 'value' => 4112, 'discount' => 25, 'installments' => 10, 'image' => 'product.png'],
-  //   ['name' => 'Título do Produto', 'rating' => 4, 'value' => 3570, 'discount' => 25, 'installments' => 10, 'image' => 'product.png'],
-  // ],
+  'Mais Vendidos' => $produtosMaisVendidos,
+  'Recomendados' => $produtosRecomendados,
+  'Novidades' => $produtosNovos,
 ];
 
 closeConnection($connection);
--- Aula 5/repo/produtos.php
+++ Aula 5/repo/produtos.php
@@ -3,8 +3,8 @@
 require_once 'lib/database.php';
 require_once 'lib/debug.php';
 
-function getProdutosEmOferta($connection) {
-    $result = mysqli_query($connection, 'select * from produtos order by desconto desc limit 6');
+function getProdutos($connection, $query) {
+    $result = mysqli_query($connection, $query);
     $produtos = fetchMany($result);
 
     foreach ($produtos as $i => $produto) {
@@ -13,6 +13,29 @@ function getProdutosEmOferta($connection) {
     }
 
     return $produtos;
+}
+
+function getProdutosEmOferta($connection) {
+    return getProdutos($connection, 'select * from produtos order by desconto desc limit 6');
+}
+
+function getProdutosMaisVendidos($connection) {
+    return getProdutos($connection, '
+        select p.*, sum(quantidade) as quantidade
+        from pedidos_produtos as pp
+        join produtos as p on p.id = pp.produto_id
+        group by produto_id
+        order by quantidade desc
+        limit 6;
+    ');
+}
+
+function getProdutosNovos($connection) {
+    return getProdutos($connection, 'select * from produtos order by criado_em desc limit 6');
+}
+
+function getProdutosRecomendados($connection) {
+    return getProdutos($connection, 'select * from produtos order by rand() desc limit 6');
 }
 
 function getAvaliacaoMedia($connection, $produtoId) {
